a5204b77e1236b13ccc46b518709064cbb70e167
Yamini <tylsaraswathi@gmail.com>
2026-07-24 18:53:01 +0530

    Updated api

--- src/api.js
+++ src/api.js
@@ -4,4 +4,31 @@ const api = axios.create({
   baseURL: "http://localhost:5000",
 });
 
+// Automatically attach JWT token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Handle Unauthorized responses
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = "/auth-login";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default api;
